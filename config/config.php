81d704c86b371d882c6a9809c8812b7eb68e9be4
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2022-02-21 16:12:00 +0530

    set channex as supported in minimal subscription extension

--- config/config.php
+++ config/config.php
@@ -8,6 +8,6 @@ $config = array(
     "image_name" => "channex_logo.svg",
     "setting_link" => "channex",
     "categories" => array("channel_manager"),
-    "is_open_source" => true,
+    "supported_in_minimal" => true,
     "marketplace_product_link" => "https://marketplace.minical.io/product/channex-integration"
 );
